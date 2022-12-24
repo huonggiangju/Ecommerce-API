e4c9c8f0b2ab8866e2b6d47c38e5e553e72eb2c2
giang <nhgiang9992@gmail.com>
2022-12-24 23:57:12 +1030

    extention token expriresIn: 300d

--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.get("/login", async(req, res)=>{
                 id: user._id,
                 isAdmin: user.isAdmin,
             }, process.env.JWT_SEC,
-             {expiresIn: "3d"});
+             {expiresIn: "300d"});
 
             const {password, ...others} = user._doc; //dont show password
             
